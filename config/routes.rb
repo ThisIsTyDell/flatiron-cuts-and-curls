df30fefd3e12cb5ccc9ac38bf444a1fda50f63f7
ThisIsTyDell <milleb94@gmail.com>
2016-10-18 09:46:00 -0500

    reroute

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  get '/users' => 'users#index'
-  get '/users/:id' => 'users#show' 
 
   devise_for :users
+
+  get '/users' => 'users#index'
+  get '/users/:id' => 'users#show' 
   root 'static#index'
 
 end
