2ab7264f6acc5c3402efc7c6ff5347cd988bba4c
ThisIsTyDell <milleb94@gmail.com>
2016-10-18 18:56:41 -0500

    add private method appointment params

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -13,4 +13,10 @@ class AppointmentsController < ApplicationController
     available ? redirect_to user_path(user) : render :new
   end
 
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:time_slot_id, :day).merge(user_id: current_user.id)
+  end
+
 end
