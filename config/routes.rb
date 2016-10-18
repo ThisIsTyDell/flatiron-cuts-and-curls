d45f6b743632b2bae324a6d3c824423c3fcc95aa
ThisIsTyDell <milleb94@gmail.com>
2016-10-18 08:38:34 -0500

    add show route, action and view to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/users' => 'users#index' 
+  get '/users' => 'users#index'
+  get '/users/:id' => 'users#show' 
 
   devise_for :users
   root 'static#index'
