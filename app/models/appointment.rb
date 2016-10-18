ea2add5120a761e94c03d9fe160046c0d8f5ef87
ThisIsTyDell <milleb94@gmail.com>
2016-10-18 15:15:44 -0500

    install appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,9 +1,10 @@
-class Appointment < ApplicationRecord
+class Appointment < ActiveRecord::Base
   belongs_to :user
   has_many :appointment_services
   has_many :services, through: :appointment_services
 
-  attr_accessible :date, :hour
-  vaidates :date, :presence => true
-  vaidates :hour, :presence => true, :uniqueness => {:scope => :date}
+  attr_accessible :date, :hour, :done
+
+  validates :date,  :presence => true
+  validates :hour,  :presence => true, :uniqueness => {:scope => :date}
 end
