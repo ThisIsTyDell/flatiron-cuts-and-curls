c7b622033cf7b00b0c9570ddb5bb916a8f43f2f2
ThisIsTyDell <milleb94@gmail.com>
2016-10-18 07:54:57 -0500

    add name and role column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018054705) do
+ActiveRecord::Schema.define(version: 20161018125036) do
 
   create_table "appointment_services", force: :cascade do |t|
     t.integer  "appointment_id"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20161018054705) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "stylist_id",             default: 1
+    t.string   "name"
+    t.integer  "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["stylist_id"], name: "index_users_on_stylist_id"
