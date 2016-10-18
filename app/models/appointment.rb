203522eb2d1e85c756d890388bbd543458406d5d
ThisIsTyDell <milleb94@gmail.com>
2016-10-18 15:01:52 -0500

    add validations and attributes to appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,8 @@ class Appointment < ApplicationRecord
   belongs_to :user
   has_many :appointment_services
   has_many :services, through: :appointment_services
+
+  attr_accessible :date, :hour
+  vaidates :date, :presence => true
+  vaidates :hour, :presence => true, :uniqueness => {:scope => :date}
 end
