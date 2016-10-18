cb6c8125a7805865ea2c37da9bafb670aa2eb6b7
ThisIsTyDell <milleb94@gmail.com>
2016-10-18 15:05:57 -0500

    add resources :appointments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root 'static#index'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users
 
   get '/users' => 'users#index'
   get '/users/:id' => 'users#show' 
-  root 'static#index'
 
+  resources :appointments
 end
