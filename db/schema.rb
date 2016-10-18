01a2ced6468c7b99de57af956398f4edefb9511c
ThisIsTyDell <milleb94@gmail.com>
2016-10-17 22:33:44 -0500

    create category model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018033007) do
+ActiveRecord::Schema.define(version: 20161018033305) do
 
   create_table "appointments", force: :cascade do |t|
     t.string   "date"
     t.string   "hour"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
