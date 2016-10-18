cbb725fbd59a56d392a4bf696b7e640ef1598de5
ThisIsTyDell <milleb94@gmail.com>
2016-10-18 01:11:20 -0500

    add method to check if user has a stylist. stylist_id: and user_id: 1 is reserved for a dummy account to return no stylist

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   has_many :appointments
   has_many :customers, class_name: "User", foreign_key: "stylist_id"
   belongs_to :stylist, class_name: "User"
+
+  def have_stylist?
+    if self.stylist_id == 1
+      return "You do not have a stylist"
+    else
+      true
+    end
+  end
 end
