975d57f00839613e31e13f06330a6bd213e00f30
ThisIsTyDell <milleb94@gmail.com>
2016-10-19 01:25:31 -0500

    add authentication to services controller and show all services on service index page

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,8 +1,9 @@
 class ServicesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   before_action :set_service, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @services = Service.all
   end
 
   def new
