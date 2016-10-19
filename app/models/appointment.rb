d799cb2a6958df5f8b007ef848923af6d511f338
ThisIsTyDell <milleb94@gmail.com>
2016-10-18 21:50:29 -0500

    add nested attributes

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,4 +3,5 @@ class Appointment < ActiveRecord::Base
   belongs_to :time_slot
   has_many :appointment_services
   has_many :services, through: :appointment_services
+  accepts_nested_attributes_for :appointment_services
 end
