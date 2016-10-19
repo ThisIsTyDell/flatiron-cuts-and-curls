05a76b1b579118af287d61b74f0a29378f83d575
ThisIsTyDell <milleb94@gmail.com>
2016-10-18 21:05:53 -0500

    add update action, set user_params and a set_user before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,32 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.where("id > 1")
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :stylist_id)
+  end
+
+  def set_user
     @user = User.find(params[:id])
   end
+
 end
