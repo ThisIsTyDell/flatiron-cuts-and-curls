5383bd1ce6f226e56df3282a534842446180ae71
ThisIsTyDell <milleb94@gmail.com>
2016-10-18 23:41:48 -0500

    add private method to set user before_action and service_params

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,11 +1,12 @@
 class ServicesController < ApplicationController
-  
+  before_action :set_service, only: [:show, :edit, :update, :destroy]
+
   def index
 
   end
 
   def new
-
+    @service = Service.new
   end
 
   def create
@@ -18,6 +19,17 @@ class ServicesController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def set_user
+    @service = Service.find(params[:id])
+  end
+
+
+  def service_params
+    params.require(:service).permit(:title, :price)
   end
 
 end
