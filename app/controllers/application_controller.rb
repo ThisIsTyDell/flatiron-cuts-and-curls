2f089fe6a7e7454fd2d052a59d6b69fcae969a1d
ThisIsTyDell <milleb94@gmail.com>
2016-10-19 15:48:15 -0500

    add error messages to application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def user_not_authorized(exception)
+    flash[:error] = "Access denied."
+    redirect_to(request.referrer || :root)
+  end
 end
