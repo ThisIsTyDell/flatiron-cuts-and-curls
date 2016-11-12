381c9807248e049117431d932efdb5d7752cce66
ThisIsTyDell <milleb94@gmail.com>
2016-11-12 16:03:20 -0600

    add missing image and default for paperclip

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,7 +2,7 @@ class Service < ApplicationRecord
   belongs_to :category
   has_many :appointment_services
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
   
   def details
