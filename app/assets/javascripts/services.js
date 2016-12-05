91ecdc190963d4c7342d26adb84a0d4ac97bc328
ThisIsTyDell <milleb94@gmail.com>
2016-12-05 14:12:08 -0600

    move binding event to document ready

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,35 +1,31 @@
 $(function () {
-  getNext();
-  getPrevious();
+  $(".js-next").on('click', getNext);
+  $(".js-previous").on('click', getPrevious);
 });
 
 function getNext() {
-  $(".js-next").on("click", function(event) {
-    event.preventDefault();
-    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
-    $.get("/services/" + nextId + ".json", function(data) {
-      
-      $(".serviceTitle").text(data["title"]);
-      $(".servicePrice").text(data["price"]);
-      $(".serviceDescription").text(data["description"]);
+  event.preventDefault();
+  var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+  $.get("/services/" + nextId + ".json", function(data) {
+    
+    $(".serviceTitle").text(data["title"]);
+    $(".servicePrice").text(data["price"]);
+    $(".serviceDescription").text(data["description"]);
 
-      $(".js-next").attr("data-id", data["id"]);
-    });
+    $(".js-next").attr("data-id", data["id"]);
   });
 }
 
 function getPrevious() {
-  $(".js-previous").on("click", function(event) {
-    event.preventDefault();
-    var previousId = parseInt($(".js-previous").attr("data-id")) - 1;
+  event.preventDefault();
+  var previousId = parseInt($(".js-previous").attr("data-id")) - 1;
 
-    $.get("/services/" + previousId + ".json", function(data) {
+  $.get("/services/" + previousId + ".json", function(data) {
 
-      $(".serviceTitle").text(data["title"]);
-      $(".servicePrice").text(data["price"]);
-      $(".serviceDescription").text(data["description"]);
+    $(".serviceTitle").text(data["title"]);
+    $(".servicePrice").text(data["price"]);
+    $(".serviceDescription").text(data["description"]);
 
-      $(".js-previous").attr("data-id", data["id"]);
-    });
+    $(".js-previous").attr("data-id", data["id"]);
   });
 };
