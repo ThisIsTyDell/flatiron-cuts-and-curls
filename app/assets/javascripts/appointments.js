ed686e5594a0100914e7448ae4f9528541da3ef3
ThisIsTyDell <milleb94@gmail.com>
2016-12-06 16:26:01 -0600

    iterate over json to return each appointment

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -4,5 +4,13 @@ $(function () {
 
 function showAppointments() {
   event.preventDefault();
-  var userId = parseInt(this.attributes[2].value)
+  var userId = parseInt(this.attributes[2].value);
+
+  $.get("/users/" + userId + "/appointments.json", function(data) {
+    var appointments = data
+    appointments.forEach(function(appointment) {
+      console.log(appointment)
+    })
+  });
+
 }
