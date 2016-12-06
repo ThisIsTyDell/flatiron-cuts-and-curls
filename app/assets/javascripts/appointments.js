43844d28762b698e3a27fdcaa956a43aa87f3dc8
ThisIsTyDell <milleb94@gmail.com>
2016-12-06 17:03:17 -0600

    append appointment text to the DOM

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -9,10 +9,13 @@ function showAppointments() {
   $.get("/users/" + userId + "/appointments.json", function(data) {
     var appointments = data
     appointments.forEach(function(appointment) {
-      var time = "<h4>" + appointment.time_slot.name + "</h4>"
+      var time = "<h4 class='dark-gray'>" + appointment.time_slot.name + "</h4>"
       var paid = "<p>Paid: " + appointment.paid + "</p>"
-      var total = "<p> Total: $" + appointment.total_price_paid + ".00</p>"
+      var total = "<p>Total: $" + appointment.total_price_paid + ".00</p>"
+      var appointmentText = time + paid + total
       
+      $("#user-" + userId + "-appointments").html(appointmentText)
+
       appointment["services"].forEach(function(service) {
         console.log(service.title)
       })
