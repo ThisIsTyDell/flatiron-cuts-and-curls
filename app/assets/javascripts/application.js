a3ba3c646a476a5df4b7f7247e74d00fda6fb696
ThisIsTyDell <milleb94@gmail.com>
2016-12-07 13:54:46 -0600

    remove unused js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require handlebars.min
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require handlebars.min
 //= require_tree .
 //= require jquery-ui
-
 //= bootstrap.min.js
 //= jquery-1.10.1.min.js
-//= main.js
 //= modernizr-2.6.2-respond-1.1.0.min.js
