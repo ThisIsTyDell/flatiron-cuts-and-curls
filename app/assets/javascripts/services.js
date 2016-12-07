2968b703172a08f3d3de324fc909875de1ff41c3
ThisIsTyDell <milleb94@gmail.com>
2016-12-07 12:54:01 -0600

    remove Service.ready and place into document ready to fix browser loading issue

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -18,11 +18,6 @@ Service.success = function(data){
   $("#serviceResult").append(serviceDiv)
 }
 
-Service.ready = function() {
-  Service.templateSource = $("#entry-template").html();
-  Service.template = Handlebars.compile(Service.templateSource);
-}
-
 Service.prototype.renderDIV = function() {
   return Service.template(this);
 }
@@ -60,5 +55,6 @@ $(function () {
   $(".js-next").on('click', getNext);
   $(".js-previous").on('click', getPrevious);
   $('.quick-add').on("submit", Service.formSubmit);
-  Service.ready();
+  Service.templateSource = $("#entry-template").html();
+  Service.template = Handlebars.compile(Service.templateSource);
 });
