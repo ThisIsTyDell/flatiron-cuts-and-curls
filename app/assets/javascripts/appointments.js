b11061b63a61d1ffd9e237d3ff1097f01f0b43c8
ThisIsTyDell <milleb94@gmail.com>
2016-12-06 18:28:29 -0600

    clean up code

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -9,20 +9,18 @@ function showAppointments() {
   $.get("/users/" + userId + "/appointments.json", function(data) {
     var appointments = data
     var services = []
+
     appointments.forEach(function(appointment) {
       var time = "<h4 class='dark-gray'>" + appointment.time_slot.name + " at " + appointment.time_slot.time + "</h4>"
       var paid = "<p>Paid: " + appointment.paid + "</p>"
       var total = "<p>Total: $" + appointment.total_price_paid + ".00</p>"
       var appointmentText = "<li>" + time + paid + total + services.join(' - ') + "</li>"
       
-      $("#user-" + userId + "-appointments").append(appointmentText)
-
       appointment["services"].forEach(function(service) {
         services.push(service.title)
-      })
-      
-
-    })
+      });
+      $("#user-" + userId + "-appointments").append(appointmentText)
+    });
   });
 
 }
